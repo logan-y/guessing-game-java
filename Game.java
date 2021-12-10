9f42ea08ed74d133522e1d127bed824b7d3c04d3
Logan Young <logany@att.net>
2021-12-10 14:39:42 -0600

    added reverse game, comp guesses users num

--- Game.java
+++ Game.java
@@ -14,12 +14,12 @@ public class Game {
     String userName = input.nextLine();
     System.out.println("Well it's about time you showed up, " + userName);
     System.out.print("Wanna play a game? All you have to do is give me a number between 1 and 100... ");
-    // create an instance of the Random object
     boolean nextGame = true;
-    int lowestTries = 15;
+    int lowestTries = 100;
     do {
       Random rand = new Random(); // create an instance of the Random object
       int number = rand.nextInt(100) + 1; // call method nextInt and pass in upper range bound
+//      System.out.println("ans is " +number);
       int count = 1;
       while (true) {
         try {
@@ -27,7 +27,7 @@ public class Game {
           if (ans < 0 || ans > 100) {
             System.out.println("Please stick to the script. 1-100 only. ");
           } else {
-            if (ans == number) {
+              if (ans == number) {
               System.out.println("Damn. You are good!");
               if (count < lowestTries) {
                 lowestTries = count;
@@ -57,5 +57,62 @@ public class Game {
       }
       System.out.println("You got it!");
     } while(nextGame);
+
+    //REVERSED GAME
+    System.out.println("Wanna play it in reverse? I'll try to guess YOUR number...1/0?");
+    int newGame = input.nextInt();
+    if(newGame == 1){
+      System.out.println("Ok, here are the rules. I guess. You tell me 'higher', 'lower', or 'you won' and eventually I will read your mind.");
+        Random rand = new Random();
+        int newGuess = rand.nextInt(100) + 1;
+        int attempts = 0;
+        int max = 100;
+        int min = 1;
+        boolean correct = false;
+        System.out.println("Is " + newGuess + " your number?");
+        String res = input.nextLine();
+        if(res.equals("you won")){
+          System.out.println("NICE");
+        } else while (!correct) {
+          String clue = input.nextLine();
+//            newGuess = rand.nextInt(100);
+          switch (clue) {
+            case "higher":
+              min = newGuess;
+              newGuess = rand.nextInt(max - min) + min;
+              System.out.printf("max is %d and min is %d..", max, min);
+              System.out.println("How about " + newGuess + "??");
+              break;
+            case "lower":
+              max = newGuess;
+              newGuess = rand.nextInt(max - min) + min;
+              System.out.printf("max is %d and min is %d..", max, min);
+              System.out.println("How about " + newGuess + "??");
+              break;
+            case "you won":
+              System.out.println("I knew it all along I was just pretending the first " + attempts + " times.");
+              correct = true;
+              break;
+            default:
+              System.out.println("Give me a better clue, yo");
+              break;
+          }
+          if (attempts == 9) {
+            System.out.println("I should have had it by now... I give up!");
+            break;
+          }
+          attempts++;
+        }
+    } else if(newGame == 0){
+      System.out.println("Thanks for playing! I hope you had fun. I sure did.");
+    } else {
+      System.out.println("I'll take that as a no? It's all good. See ya!");
+    }
+
+
+
+
+
+
   }
 }
